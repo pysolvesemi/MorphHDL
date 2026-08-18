06cd6a2af078dd83897921b16524af85388633eb
pysolvesemi <pysolvesemi@gmail.com>
2026-08-18 14:22:51 +0530

    test: align symbolic data-shape golden with native declaration order

--- morphhdl/examples/contracts/symbolic_data_shapes.v
+++ morphhdl/examples/contracts/symbolic_data_shapes.v
@@ -1,60 +1,60 @@
 module SymbolicDataShapes #(
   parameter integer WIDTH = 8
 ) (
-  input  wire [WIDTH-1:0] bits_in,
-  input  wire [WIDTH-1:0] bundle_in_bits,
-  input  wire [WIDTH-1:0] bundle_in_sint,
-  input  wire [WIDTH-1:0] bundle_in_uint,
-  input  wire          clk,
-  input  wire [WIDTH-1:0] flow_in_payload_bits,
-  input  wire [WIDTH-1:0] flow_in_payload_sint,
-  input  wire [WIDTH-1:0] flow_in_payload_uint,
-  input  wire          flow_in_valid,
-  input  wire [WIDTH-1:0] sint_in,
-  input  wire [WIDTH-1:0] stream_in_payload_bits,
-  input  wire [WIDTH-1:0] stream_in_payload_sint,
-  input  wire [WIDTH-1:0] stream_in_payload_uint,
+  input  wire [WIDTH-1:0]    bits_in,
+  output wire [WIDTH-1:0]    bits_out,
+  input  wire [WIDTH-1:0]    uint_in,
+  output wire [WIDTH-1:0]    uint_out,
+  input  wire [WIDTH-1:0]    sint_in,
+  output wire [WIDTH-1:0]    sint_out,
+  input  wire [WIDTH-1:0]    bundle_in_bits,
+  input  wire [WIDTH-1:0]    bundle_in_uint,
+  input  wire [WIDTH-1:0]    bundle_in_sint,
+  output wire [WIDTH-1:0]    bundle_out_bits,
+  output wire [WIDTH-1:0]    bundle_out_uint,
+  output wire [WIDTH-1:0]    bundle_out_sint,
+  input  wire [WIDTH-1:0]    vec_in_0_bits,
+  input  wire [WIDTH-1:0]    vec_in_0_uint,
+  input  wire [WIDTH-1:0]    vec_in_0_sint,
+  input  wire [WIDTH-1:0]    vec_in_1_bits,
+  input  wire [WIDTH-1:0]    vec_in_1_uint,
+  input  wire [WIDTH-1:0]    vec_in_1_sint,
+  output wire [WIDTH-1:0]    vec_out_0_bits,
+  output wire [WIDTH-1:0]    vec_out_0_uint,
+  output wire [WIDTH-1:0]    vec_out_0_sint,
+  output wire [WIDTH-1:0]    vec_out_1_bits,
+  output wire [WIDTH-1:0]    vec_out_1_uint,
+  output wire [WIDTH-1:0]    vec_out_1_sint,
   input  wire          stream_in_valid,
-  input  wire          stream_out_ready,
-  input  wire [WIDTH-1:0] uint_in,
-  input  wire [WIDTH-1:0] vec_in_0_bits,
-  input  wire [WIDTH-1:0] vec_in_0_sint,
-  input  wire [WIDTH-1:0] vec_in_0_uint,
-  input  wire [WIDTH-1:0] vec_in_1_bits,
-  input  wire [WIDTH-1:0] vec_in_1_sint,
-  input  wire [WIDTH-1:0] vec_in_1_uint,
-  output wire [WIDTH-1:0] bits_out,
-  output wire [WIDTH-1:0] bundle_out_bits,
-  output wire [WIDTH-1:0] bundle_out_sint,
-  output wire [WIDTH-1:0] bundle_out_uint,
-  output wire [WIDTH-1:0] flow_out_payload_bits,
-  output wire [WIDTH-1:0] flow_out_payload_sint,
-  output wire [WIDTH-1:0] flow_out_payload_uint,
-  output wire          flow_out_valid,
-  output wire [WIDTH-1:0] register_out_bits,
-  output wire [WIDTH-1:0] register_out_sint,
-  output wire [WIDTH-1:0] register_out_uint,
-  output wire [WIDTH-1:0] sint_out,
   output wire          stream_in_ready,
-  output wire [WIDTH-1:0] stream_out_payload_bits,
-  output wire [WIDTH-1:0] stream_out_payload_sint,
-  output wire [WIDTH-1:0] stream_out_payload_uint,
+  input  wire [WIDTH-1:0]    stream_in_payload_bits,
+  input  wire [WIDTH-1:0]    stream_in_payload_uint,
+  input  wire [WIDTH-1:0]    stream_in_payload_sint,
   output wire          stream_out_valid,
-  output wire [WIDTH-1:0] uint_out,
-  output wire [WIDTH-1:0] vec_out_0_bits,
-  output wire [WIDTH-1:0] vec_out_0_sint,
-  output wire [WIDTH-1:0] vec_out_0_uint,
-  output wire [WIDTH-1:0] vec_out_1_bits,
-  output wire [WIDTH-1:0] vec_out_1_sint,
-  output wire [WIDTH-1:0] vec_out_1_uint
+  input  wire          stream_out_ready,
+  output wire [WIDTH-1:0]    stream_out_payload_bits,
+  output wire [WIDTH-1:0]    stream_out_payload_uint,
+  output wire [WIDTH-1:0]    stream_out_payload_sint,
+  input  wire          flow_in_valid,
+  input  wire [WIDTH-1:0]    flow_in_payload_bits,
+  input  wire [WIDTH-1:0]    flow_in_payload_uint,
+  input  wire [WIDTH-1:0]    flow_in_payload_sint,
+  output wire          flow_out_valid,
+  output wire [WIDTH-1:0]    flow_out_payload_bits,
+  output wire [WIDTH-1:0]    flow_out_payload_uint,
+  output wire [WIDTH-1:0]    flow_out_payload_sint,
+  input  wire          clk,
+  output wire [WIDTH-1:0]    register_out_bits,
+  output wire [WIDTH-1:0]    register_out_uint,
+  output wire [WIDTH-1:0]    register_out_sint
 );
 
-  wire       [WIDTH-1:0] internal_payload_bits;
-  wire       [WIDTH-1:0] internal_payload_sint;
-  wire       [WIDTH-1:0] internal_payload_uint;
-  reg        [WIDTH-1:0] payload_register_bits;
-  reg        [WIDTH-1:0] payload_register_sint;
-  reg        [WIDTH-1:0] payload_register_uint;
+  wire       [WIDTH-1:0]    internal_payload_bits;
+  wire       [WIDTH-1:0]    internal_payload_uint;
+  wire       [WIDTH-1:0]    internal_payload_sint;
+  reg        [WIDTH-1:0]    payload_register_bits;
+  reg        [WIDTH-1:0]    payload_register_uint;
+  reg        [WIDTH-1:0]    payload_register_sint;
 
   assign bits_out = bits_in;
   assign uint_out = uint_in;
