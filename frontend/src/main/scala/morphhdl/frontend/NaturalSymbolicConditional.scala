6b772cabbfdaaf73ca97d8c21fc3d8b188c377fa
pysolvesemi <pysolvesemi@gmail.com>
2026-08-21 01:30:59 +0530

    Remove macro from Increment 48 symbolic helper

--- frontend/src/main/scala/morphhdl/frontend/NaturalSymbolicConditional.scala
+++ frontend/src/main/scala/morphhdl/frontend/NaturalSymbolicConditional.scala
@@ -1,8 +1,5 @@
 package morphhdl.frontend
 
-import scala.language.experimental.macros
-import scala.reflect.macros.whitebox
-
 /** Typed bridge introduced by the MorphHDL compiler plugin for natural Scala `if` syntax. */
 object NaturalSymbolicConditional {
   /**
@@ -10,10 +7,7 @@ object NaturalSymbolicConditional {
     * Ordinary Scala Boolean conditionals are deliberately left as raw Scala `if`
     * expressions by the compiler plugin and never pass through this method.
     */
-  def selectSymbolic[T](condition: HdlBool)(ifTrue: => T)(ifFalse: => T): T =
-    macro NaturalSymbolicConditionalMacro.selectSymbolicImpl[T]
-
-  private[frontend] def runtime[T](
+  def selectSymbolic[T](
       condition: HdlBool,
       sourceFile: String,
       sourceLine: Int
@@ -29,43 +23,5 @@ object NaturalSymbolicConditional {
       else falseValue.get
     } else if (condition.witness) ifTrue
     else ifFalse
-  }
-}
-
-object NaturalSymbolicConditionalMacro {
-  def selectSymbolicImpl[T: c.WeakTypeTag](
-      c: whitebox.Context
-  )(
-      condition: c.Expr[HdlBool]
-  )(
-      ifTrue: c.Expr[T]
-  )(
-      ifFalse: c.Expr[T]
-  ): c.Expr[T] = {
-    import c.universe._
-
-    object UnsafeAlternativeEffect extends Traverser {
-      var finding: Option[(Position, String)] = None
-      override def traverse(tree: Tree): Unit = if (finding.isEmpty) tree match {
-        case Return(_) => finding = Some(tree.pos -> "return")
-        case Throw(_)  => finding = Some(tree.pos -> "throw")
-        case _         => super.traverse(tree)
-      }
-    }
-    UnsafeAlternativeEffect.traverse(ifTrue.tree)
-    UnsafeAlternativeEffect.traverse(ifFalse.tree)
-    UnsafeAlternativeEffect.finding.foreach { case (position, effect) =>
-      c.abort(
-        position,
-        s"MORPHDL-SYMBOLIC-CONDITIONAL-UNSAFE-EFFECT: '$effect' is not supported inside an explicit symbolic alternative"
-      )
-    }
-
-    val pos = c.enclosingPosition
-    val sourceFile = Option(pos.source).map(_.path).filter(_.nonEmpty).getOrElse("<symbolic-if>")
-    val sourceLine = math.max(1, pos.line)
-    c.Expr[T](
-      q"_root_.morphhdl.frontend.NaturalSymbolicConditional.runtime[${weakTypeOf[T]}](${condition.tree}, $sourceFile, $sourceLine)(${ifTrue.tree})(${ifFalse.tree})"
-    )
   }
 }
